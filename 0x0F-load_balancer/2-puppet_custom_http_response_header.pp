00a41d29f3e65d12326b2ab7588f8f63a70dbdce
MaxiHBTN <4387@holbertonschool.com>
2022-08-22 19:19:53 +0000

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,8 @@
 # Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
 
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+exec { 'apt-get update':
+  command  => 'apt-get update',
+  provider => shell,
 }
 
 package { 'nginx':
@@ -14,11 +15,6 @@ file_line { 'Header response':
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World',
   require => Package['nginx'],
 }
 
