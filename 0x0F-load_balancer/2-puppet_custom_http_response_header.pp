0ffdefea20c8f2daac196c4449e106b271d9bd47
MaxiHBTN <4387@holbertonschool.com>
2022-08-22 18:40:29 +0000

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
 
-package { 'haproxy':
+package { 'nginx':
 }
  
 file_line { 'HTTP header':
