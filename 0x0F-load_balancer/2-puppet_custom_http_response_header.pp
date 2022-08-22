0b472a20fbe205006bc4a4fd66f35188877bcccd
MaxiHBTN <4387@holbertonschool.com>
2022-08-22 18:57:15 +0000

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,7 +7,7 @@ file_line { 'Header response':
   ensure   => present,
   path     => '/etc/nginx/sites-available/default',
   after    => 'server_name _;',
-  line     => "add_header X-Served-By ${hostname};",
+  line     => 'add_header X-Served-By $hostname',
 }
 
 service { 'nginx':
