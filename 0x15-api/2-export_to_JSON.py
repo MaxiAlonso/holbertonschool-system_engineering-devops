d93f6a5f49251defff9b12289daa21b1ea657cf4
MaxiHBTN <4387@holbertonschool.com>
2022-09-06 18:08:44 +0000

    Task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Sscript to export data in the JSON format.
+"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    usrId = int(argv[1])
+    todo_api_url = "https://jsonplaceholder.typicode.com/todos"
+    todo_response = requests.get(todo_api_url).json()
+    user_api_url = "https://jsonplaceholder.typicode.com/users/{}".\
+        format(usrId)
+    user_response = requests.get(user_api_url).json()
+    task_list = []
+    for todo in todo_response:
+        list_dict = {}
+        if todo.get("userId") == usrId:
+            list_dict["task"] = todo.get("title")
+            list_dict["completed"] = todo.get("completed")
+            list_dict["username"] = user_response.get("username")
+            task_list.append(list_dict)
+        new_dict = {usrId: task_list}
+    with open("{}.json".format(usrId), mode='w') as file:
+        json.dump(new_dict, file)
