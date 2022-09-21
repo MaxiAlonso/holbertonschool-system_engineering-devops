b441e88950daa13fa20735669750a70448957432
MaxiHBTN <4387@holbertonschool.com>
2022-09-21 15:04:09 +0000

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Task 1 - 0x16. API advanced
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API
+    and prints the titles of the first 10
+    hot posts listed for a given subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    # Get a copy of the default headers that requests would use
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'})
+    response = requests.get(url, headers=headers).json()
+    posts = response.get('data').get('children')
+    if posts:
+        for post in posts:
+            print(post.get('data').get('title'))
+    else:
+        print(None)
