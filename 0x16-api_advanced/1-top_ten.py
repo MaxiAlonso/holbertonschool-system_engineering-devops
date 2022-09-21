3c8ab95c3ca293ade5e7ec66218f11667ce5a37e
MaxiHBTN <4387@holbertonschool.com>
2022-09-21 15:28:25 +0000

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -18,9 +18,9 @@ def top_ten(subreddit):
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'})
-    response = requests.get(url, headers=headers).json()
-    posts = response.get('data').get('children')
-    if posts:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        posts = response.json().get('data').get('children')
         for post in posts:
             print(post.get('data').get('title'))
     else:
