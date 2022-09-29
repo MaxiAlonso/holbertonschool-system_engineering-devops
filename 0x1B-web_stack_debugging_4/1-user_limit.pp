8b7194b68c2b265506ffe2e95e93bc710f5559c5
MaxiHBTN <4387@holbertonschool.com>
2022-09-29 14:08:49 +0000

    Task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Task 1
+
+
+exec {'hard':
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/g" /etc/security/limits.config',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+exec {'soft':
+  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 5000/g" /etc/security/limits.config',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  }
